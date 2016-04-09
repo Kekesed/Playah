417e2ced11407e2e866ce5641867fa3653e1802e
Kekesed <iam@kekesed.gq>
2016-04-09 14:03:06 +0700

    Playah Revisi 2! Mantep ANJENG, ada soundcloud-integrationnya >.<

--- js/kekesed.playah.js
+++ js/kekesed.playah.js
@@ -6,7 +6,7 @@
    https://kekesed.id
 
    @author: Mealkuwat // Kekesed
-   @copyright: Copyright (c) 2016 Kekesed
+   @copyright: Copyright (c) 2016 Kekesed. Some rights reserved.
    @link: https://kekesed.id/
 
 
@@ -19,6 +19,7 @@ $.widget("Kekesed.Playah", {
 		random:false,
 		loop:true,
 		android_mode:true,
+    sc_id:undefined,
 		playlist: [],
     /*
       Playlist.json:
@@ -27,9 +28,8 @@ $.widget("Kekesed.Playah", {
       path:'/ganteng.mp3'
       meta:{picture:asd}
     },{
-      type:'soundcloud:playlist',
+      type:'soundcloud',
       path:'https:',
-      behavior: function(songid,songmetadata){}
       }]
     */
 		theme: {
@@ -37,12 +37,12 @@ $.widget("Kekesed.Playah", {
       accent:'darkBlue'
     },
     header:{
-      height:'150px'
+      height:'150px',
+      options: {'data-role':'fitImage', 'data-type':'sd'}
     },
 		callback:{
 			onNext:function(SongData) {},
 			onPrev:function(SongData) {},
-			onState:function(Status, SongData) {},
 			onError:function(Err, SongData) {},
 			onLoad:function() {},
 		},
@@ -101,6 +101,15 @@ $.widget("Kekesed.Playah", {
     bdn.kBckw.append(bdn.iBack);
     bdn.kFrwd.append(bdn.iForw);
     bdn.subjudul.append(bdn.artis).append("&nbsp;&nbsp;").append(bdn.album);
+
+    //listener
+    bdn.album.click(function(e){
+      e.preventDefault();
+      if($(this).data('url'))
+        //#Done:10 open new tab on click to masta
+        window.open($(this).data('url'), "_blank");
+    });
+
     bdn_.append(bdn.judul)
         .append(bdn.subjudul)
         .append(bdn.kBckw)
@@ -109,23 +118,27 @@ $.widget("Kekesed.Playah", {
     this.element.append(this.statik.kontrol.skeleton.kepala)
                 .append(this.statik.kontrol.skeleton.progress)
                 .append(this.statik.kontrol.skeleton.badan);
+
+
+    this.sc.data.clid = this.options.sc_id;
     // init finish
     this.options.callback.onLoad();
+    this.element.trigger('loaded');
+
 
     //PRE-CYCLE PREPARATION
     var opt = this.options;
     var par = this;
     this.resolve_playlist().then(function(){
       //AUTOPLAY
-      console.log(par.statik.siklus.Playmate);
+      //console.log(par.statik.siklus.Playmate);
       par._next(true);
-      if(opt.autoplay){
+      if(opt.autoplay)
         par._emit();
-      } else {
-        par._emit(true); // autoplay disabled
-      }
+      else
+        par._emit(true);
+
       par.update_kiri_kanan();
-      //BUTTON BINDER
       var sket = par.statik.kontrol.badan;
       $(sket.kPlay).click(function(){
         if(!par.statik.cache.isplaying)
@@ -145,19 +158,20 @@ $.widget("Kekesed.Playah", {
         var letakDurasi = $(par.statik.cache.player).prop('duration') * persen;
         $(par.statik.cache.player)[0].currentTime =letakDurasi;
       });
+    }).catch(function(err){
+      par._trigger('error', [err]);
+      par.options.callback.onError(err, Object.assign({},par.statik.siklus.curr_audio));
     });
   },
 
   update_kiri_kanan: function(){
     var opt = this.options;
     if(this.statik.siklus.Playmate.length <= 0) {
-      //nggak ada track di playlistnya
       $(this.statik.kontrol.badan.kBckw).prop('disabled', true);
       $(this.statik.kontrol.badan.kFrwd).prop('disabled', true);
       $(this.statik.kontrol.badan.kPlay).prop('disabled', true);
       return;
     } else if(this.statik.siklus.Playmate.length == 1) {
-      //cuma atu... disabling the prev and next button
       $(this.statik.kontrol.badan.kBckw).prop('disabled', true);
       $(this.statik.kontrol.badan.kFrwd).prop('disabled', true);
       $(this.statik.kontrol.badan.kPlay).prop('disabled', false);
@@ -177,23 +191,25 @@ $.widget("Kekesed.Playah", {
       if(Array.isArray(opt.playlist)){
 
         opt.playlist.forEach(function(dat){
-          //console.log(dat);
           if(dat.type == 'soundcloud'){
             sc=true;
-            par.sc.trackify(dat, function(kembalian){
-              if(Array.isArray(kembalian)){
-                kembalian.forEach(function(da){
-                  crrPlaylist.push(da);
-                });
-              } else
-                crrPlaylist.push(kembalian);
+            par.sc.trackify(dat).then(function(kembalian){
+              if(kembalian)
+                if(Array.isArray(kembalian)){
+                  kembalian.forEach(function(da){
+                    crrPlaylist.push(da);
+                  });
+                } else
+                  crrPlaylist.push(kembalian);
 
-
-                if (first)
+                if(first)
                   ok();
                 first=false;
 
                 par.update_kiri_kanan();
+            }).catch(function(errz){
+              //error happend.
+              err(errz);
             });
           } else{
             crrPlaylist.push(dat);
@@ -218,11 +234,17 @@ $.widget("Kekesed.Playah", {
 
   sc: {
     data: {
-      clid: 'b9d1369138efad91c7ab558b02baa3cc'
+      clid: undefined //#Done:30 bikin usernya yang massukin sendiri.
     },
-    trackify: function(data, cl){
+    trackify: function(data){
       var par = this;
-      this.resolve('resolve', {url:data.path}, function(datz){
+      return new Promise(function(ok,errz){
+      if (par.data.clid == undefined || par.data.clid == '') {
+        errz({context:'cycle.soundloud-integration', detail:'Client ID are unsetted'});
+        return;
+      }
+      //var par = this;
+      par.resolve('resolve', {url:data.path}).then(function(datz){
         if (datz.uri.search('track') != -1)
           var ret ={
             type:'soundcloud',
@@ -253,30 +275,33 @@ $.widget("Kekesed.Playah", {
           });
         }
 
-        cl(ret);
+        ok(ret);
+      }).catch(function(err){
+        errz({context:'cycle.soundcloud-integration', detail:'Ajax Error, something happend. See Error.debugData', debugData:err});
       });
-      return ;
+    });
     },
-    resolve: function(url, data, cl){
-      var dat = data;
-      dat.client_id = this.data.clid;
+    resolve: function(url, data){
+      var dat = Object.assign({client_id:this.data.clid},data);
+      return new Promise(function(ok,err){
       $.ajax({
         url:'https://api.soundcloud.com/' + url,
         data: dat,
         method:'get',
       }).always(function(data){
-        cl(data);
-        return data;
+        ok(data);
+      }).fail(function(errz){
+        err(errz);
       });
+    });
     }
   },
 
   // Berguna untuk memulai siklus pemutaran lagu baru
-  _emit: function(prepare) {
+  _emit:function(prepare) {
     prepare = prepare || false;
     this._sodok_audio(this.statik.siklus.curr_audio);         //Ganti Audio
     this._ganti_judul(this.statik.siklus.curr_audio.meta);    //Ganti Judul
-    //ganti bgnya belon
 
     //PLAY
     if(this.statik.cache.isplaying || (!prepare && this.statik.cache.isplaying))
@@ -290,12 +315,23 @@ $.widget("Kekesed.Playah", {
     var knt = this.statik.kontrol.badan;
     knt.judul.text(data.title);
     knt.artis.text(data.artist);
-    knt.album.text(data.album);
+    knt.album.text('');
+    if(data.contribute) {
+      knt.album.append($('<span/>', {class:'mif-soundcloud mif-2x fg-orange'})).append('&nbsp;');
+      knt.album.addClass('sc-contribute').css({cursor:'pointer'});
+      knt.album.data('url', data.contribute);
+    } else {
+      knt.album.removeClass('sc-contribute');
+      knt.album.data('url', '').css({cursor:'pointer'});
+    }
+    knt.album.append(data.album);
 
     //ganti gambhar
     var kp = this.statik.kontrol.skeleton.kepala;
+
+    // #Done:40 Bikin jadi customizeable
     if(data.image)
-      $(kp).html('').append($('<img/>', {src:data.image, 'data-role':'fitImage', 'data-type':'sd'}));
+      $(kp).html('').append($('<img/>', Object.assign({src:data.image}, this.options.header.options)));
     else
       $(kp).html('');
   },
@@ -307,9 +343,7 @@ $.widget("Kekesed.Playah", {
       $(this.statik.cache.player).trigger('pause');
     this.statik.cache.isplaying = curr;
     this.statik.cache.player = $('<audio/>', {type:'audio/mp3',src:data.path, autoplay:false, control:false});
-
     $(this.statik.cache.player).trigger('load');
-
   },
 
   //bertanggung jawab menginject listener yang baru untuk tiap kali audio berganti,
@@ -318,16 +352,13 @@ $.widget("Kekesed.Playah", {
     var par = this;
     var ply= false;
     $(this.statik.cache.player).on({
-      /*'canplay':function(){
-        par.statik.cache.player.trigger('play');
-      },*/
       'timeupdate':function(){
-        // ganti progressbar;
-        /*ply=true;
-        par.statik.cache.isplaying = ply;*/
         $(par.statik.kontrol.skeleton.progress).data('progress').set($(this).prop('currentTime')/$(this).prop('duration')*100);
       },
       'ended':function(){
+        //#Done:0 Trigger Event Ended
+        par.element.trigger('ended', [par.statik.siklus.curr_audio]);
+
         if(par.statik.siklus.Playmate.length > 1) {
           par.statik.cache.isplaying = true;
             par._next();
@@ -340,31 +371,46 @@ $.widget("Kekesed.Playah", {
         ply=true;
         par.statik.cache.isplaying = ply;
         par.statik.kontrol.badan.kPlay.html('').append(par.statik.kontrol.badan.iPaus);
+
+        //#Doing:10 Trigger Event Playing
+        par.element.trigger('playing', [par.statik.siklus.curr_audio]);
       },
       'play': function(){
         ply=true;
         par.statik.cache.isplaying = ply;
         par.statik.kontrol.badan.kPlay.html('').append(par.statik.kontrol.badan.iPaus);
+
+        //#Doing:0 Trigger Event Play
+        par.element.trigger('play', [par.statik.siklus.curr_audio]);
       },
       'pause':function(){
         ply=false;
         par.statik.cache.isplaying = ply;
         par.statik.kontrol.badan.kPlay.html('').append(par.statik.kontrol.badan.iPlay);
+
+        //#Doing:20 Trigger Event pause
+        par.element.trigger('pause',[par.statik.siklus.curr_audio]);
       },
       'seeking':function(){
-        ply=false; // review lagi
+        ply=false; //#Review:0 review lagi
         par.statik.cache.isplaying = ply;
         par.statik.kontrol.badan.kPlay.html('').append(par.statik.kontrol.badan.iPlay);
+
+        //#Doing:30 Trigger Event seeking
+        par.element.trigger('seeking',[par.statik.siklus.curr_audio]);
       },
       'waiting':function(){
         ply=false;
         par.statik.cache.isplaying = ply;
         par.statik.kontrol.badan.kPlay.html('').append(par.statik.kontrol.badan.iPlay);
       },
-      'error':function(){
+      'error':function(ev){
         ply=false;
         par.statik.cache.isplaying = ply;
         par.statik.kontrol.badan.kPlay.html('').append(par.statik.kontrol.badan.iPlay);
+
+        //#Done:20 Terigger error event
+        par.element.trigger('error', [{context:'player.html5-integration', description:'Unknown Error on Player', detail:ev}, par.statik.siklus.curr_audio]);
       }
 
     });
@@ -373,16 +419,23 @@ $.widget("Kekesed.Playah", {
   //bertanggung jwab untuk mengganti lagu, dan memulai siklus baru
   _next:function(cancel_emit) {
     cancel_emit = cancel_emit || false;
-    console.log(this.statik.siklus.Playmate);
+    //console.log(this.statik.siklus.Playmate);
     var cnum = this.statik.siklus.curr_num;
-    cnum = (cnum+1)%this.statik.siklus.Playmate.length;
-    this.statik.siklus.curr_audio = this.statik.siklus.Playmate[cnum]; //add up the next part.
-    this.statik.siklus.curr_num = cnum;
-    console.info("Next! Current number: " + cnum);
-    console.log(this.statik.siklus.curr_audio);
-    //emit the "next are triggered."
-    if(!cancel_emit)
-      this._emit();
+    if((cnum >= this.statik.siklus.Playmate.length-1) && !this.options.loop) {
+      // udah di atas, nggak boleh loop...
+
+    } else {
+      cnum = (cnum+1)%this.statik.siklus.Playmate.length;
+      this.statik.siklus.curr_audio = this.statik.siklus.Playmate[cnum]; //add up the next part.
+      this.statik.siklus.curr_num = cnum;
+      //console.info("Next! Current number: " + cnum);
+      //console.log(this.statik.siklus.curr_audio);
+      //emit the "next are triggered."
+      //#Done:50 integrate callback event
+      this.element.trigger('next');
+      if(!cancel_emit)
+        this._emit();
+    }
   },
 
   //bertanggung jawab untuk menggantu lagu dan memulai siklus baru
@@ -395,7 +448,7 @@ $.widget("Kekesed.Playah", {
       }
     };
     var cnum = this.statik.siklus.curr_num;
-    if(cnum <= 0)
+    if(cnum <= 0 && this.options.loop)
       cnum = this.statik.siklus.Playmate.length-1;
     else
       cnum--;
@@ -405,6 +458,8 @@ $.widget("Kekesed.Playah", {
 
     console.info("Prev! Current number: " + cnum);
     console.log(this.statik.siklus.curr_audio);
+    //#Done:60 Trigger Event Prev
+    this.element.trigger('prev', [par.statik.siklus.curr_audio]);
     this._emit();
   },
 
@@ -422,6 +477,6 @@ $.widget("Kekesed.Playah", {
 
 $.fn.extend({
 	ksd_info: {
-		Playah: function() { return "2.0";},
+		Playah:"2.0",
 	}
 });
